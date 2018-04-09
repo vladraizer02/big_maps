082f0d5abbc940584c23ac1143bb6594f43c139e
vladraizer02 <vladraizer02@yandex.ru>
2018-04-09 21:49:11 +0300

    pereklychatel

--- main.py
+++ main.py
@@ -2,25 +2,37 @@ import pygame
 import requests
 import sys
 import os
+import GUII
 
 koord = '2.292771,48.858583'
 koord1 = koord.split(',')
 spn = 0.00619
-l = 'sat'
+l = 'map'
 response = None
+step = 0.005
+
+gui = GUII.GUI()
+gui.add_element(GUII.Button((350,0,90,35),"Смена"))
 
 def izmen_mashtab(znak, delta):
+    global step
     if znak == '+':
-        delta = delta + 0.005
+        delta = delta + step
     else:
-        delta = delta - 0.005
+        delta = delta - step
     return delta
 
 
 while True:
     try:
-        map_request = "https://static-maps.yandex.ru/1.x/?l=map&ll="+koord+'&size=450,450&spn='+str(spn)+','+str(spn)+'&l='+l
-        response = requests.get(map_request)
+        map_request = "https://static-maps.yandex.ru/1.x/"
+        params = {
+            "ll": koord,
+            "spn": ",".join([str(spn), str(spn)]),
+            "l": l}
+
+
+        response = requests.get(map_request, params=params)
 
         if not response:
             print("Ошибка выполнения запроса:")
@@ -30,7 +42,6 @@ while True:
     except:
         print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
         sys.exit(1)
-    # Запишем полученное изображение в файл.
     map_file = "map.png"
     try:
         with open(map_file, "wb") as file:
@@ -44,45 +55,54 @@ while True:
     screen = pygame.display.set_mode((450, 450))
     # Рисуем картинку, загружаемую из только что созданного файла.
     screen.blit(pygame.image.load(map_file), (0, 0))
+    gui.render(screen)
     # Переключаем экран и ждем закрытия окна.
-    pygame.display.flip()   
+    pygame.display.flip()
     running = True
     while running:
-        for event in pygame.event.get(): 
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                x, y = event.pos
+                if (440 > x > 350) and (35 > y > 0):
+                    if l == 'map':
+                        l = 'sat'
+                    elif l == 'sat':
+                        l = 'skl'
+                    else:
+                        l = 'map'
+                    running = False
             if event.type == pygame.KEYDOWN:
-                #Меняем масштаб
+                # Меняем масштаб
                 if event.key == 280:
                     if spn < 65:
                         spn = izmen_mashtab('+', spn)
                         running = False
                 elif event.key == 281:
                     if spn > 0.002:
-                        spn = izmen_mashtab('-', spn)   
+                        spn = izmen_mashtab('-', spn)
                         running = False
-                #Меняем центр карты
+                # Меняем центр карты
                 if event.key == 276:
                     if float(koord1[1]) > -179:
-                        koord = str(float(koord1[0])-10.005)+','+koord1[1]
+                        koord = str(float(koord1[0]) - step) + ',' + koord1[1]
                         koord1 = koord.split(',')
                         running = False
                 elif event.key == 275:
                     if float(koord1[1]) < 179:
-                        koord = str(float(koord1[0])+0.005)+','+koord1[1]
-                        koord1 = koord.split(',')       
+                        koord = str(float(koord1[0]) + step) + ',' + koord1[1]
+                        koord1 = koord.split(',')
                         running = False
                 elif event.key == 274:
                     if float(koord1[1]) > -89:
-                        koord = koord1[0]+','+str(float(koord1[1])-0.005)
-                        koord1 = koord.split(',')    
+                        koord = koord1[0] + ',' + str(float(koord1[1]) - step)
+                        koord1 = koord.split(',')
                         running = False
                 elif event.key == 273:
                     if float(koord1[1]) < 89:
-                        koord = koord1[0]+','+str(float(koord1[1])+0.005)
-                        koord1 = koord.split(',')  
+                        koord = koord1[0] + ',' + str(float(koord1[1]) + step)
+                        koord1 = koord.split(',')
                         running = False
-                    
-           
+
 pygame.quit()
- 
-# Удаляем за собой файл с изображением.
+
 os.remove(map_file)
